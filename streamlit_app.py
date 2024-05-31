e3a1e39b1315451aaba4dfd1181ac46e3c305bd2
saml-art <152212836+saml-art@users.noreply.github.com>
2024-05-31 16:43:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write('The name of your smoothie will be: '+name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('SEARCH_ON') )
-st.dataframe(data=my_dataframe, use_container_width=true)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list =st.multiselect ('Choose up to 5 ingredients: ', my_dataframe, max_selections=5)
