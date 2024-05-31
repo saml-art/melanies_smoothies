89356126538aa7b3c7fbc646a9ebd0b432736d02
saml-art <152212836+saml-art@users.noreply.github.com>
2024-05-31 15:54:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import streamlit as st
 # Write directly to the app
 st.title("Customize your Smoothie! :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom smoothie!""")
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name of your smoothie will be: '+name_on_order)
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
